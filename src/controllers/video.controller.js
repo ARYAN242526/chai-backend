d51c85c2bff314d993a6adc41f33a57ec80e22c9
ARYAN242526 <roy2503aryan@gmail.com>
2025-05-15 19:40:25 +0530

    publish video controller done

---
+++ src/controllers/video.controller.js
@@ -0,0 +1,53 @@
+import mongoose , {isValidObjectId} from "mongoose";
+import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+
+
+const publishAVideo = asyncHandler(async(req,res) => {
+    const {title , description} = req.body;
+    
+    if(!title && !description){
+        throw new ApiError(400, 'Title and description both are needed')
+    }
+
+    const videoFilePath = req.files?.videoFile[0]?.path;
+    const thumbNailFilePath = req.files?.thumbnail[0]?.path;
+
+    // console.log("VideoLocalPath : " , videoFilePath);
+    // console.log("ThumbNailLocalPath : " , thumbNailFilePath);
+
+    if(!videoFilePath && !thumbNailFilePath){
+        throw new ApiError(400 , 'Both video and thumbnail file path needed');
+    }
+
+    const cloudinaryVideoLink = await uploadOnCloudinary(videoFilePath);
+    const cloudinaryThumbnailLink = await uploadOnCloudinary(thumbNailFilePath)
+
+    // console.log("CloudinaryVideoLink" , cloudinaryVideoLink);
+    // console.log("CloudinaryThymbnailLink" , cloudinaryThumbnailLink);
+
+    const video = await Video.create({
+        videoFile : cloudinaryVideoLink.url,
+        thumbnail : cloudinaryThumbnailLink.url,
+        title,
+        description,
+        owner : req.user,
+        duration : cloudinaryVideoLink.duration
+    })
+
+    if(!video){
+        throw new ApiError(400 , 'Error while creating video')
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200 , video ,  'Video created successfully'))
+    
+})
+
+export {publishAVideo}
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -12,7 +12,7 @@ const videoSchmea = new Schema(
             type : String , // cloudinary url
             required : true
         },
-        ttle: {
+        title: {
             type : String , 
             required : true
         },
