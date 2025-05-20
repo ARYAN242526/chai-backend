7cdfa88e8e0de3a82ee6709e38b4f3c061eb368f
ARYAN242526 <roy2503aryan@gmail.com>
2025-05-20 12:51:54 +0530

    get video comments controller endpoint done

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -2,6 +2,66 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Comment } from "../models/comment.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose from "mongoose";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+    //TODO: get all comments for a video
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query;
+    const userId = req.user._id;
+
+    const skip = (page - 1) * limit;
+
+    const videoComments = await Comment.aggregate([
+        {
+            $match : {
+                video : new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup : {
+                from : "users",
+                localField : "owner",
+                foreignField : "_id",
+                as : "ownerDetails",
+                pipeline : [
+                    {
+                        $project : {
+                            username : 1,
+                            avatar : 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields : {
+                commenterOfVideo : {
+                 $first : "$ownerDetails"
+                }
+            }
+        },
+        { $sort : {createdAt : -1} },
+        { $skip : skip},
+        {$limit : parseInt(limit)},
+        {
+            $project : {
+                commenterOfVideo : 1,
+                content : 1,
+                owner : 1
+            }
+        }
+    ])
+
+    if(!videoComments || videoComments.length === 0){
+        throw new ApiError(404, "Video comments not found")
+    }
+
+    return res
+             .status(200)
+             .json(new ApiResponse(200, videoComments ,"Video Comments found successfully"))
+})
+
 
 const addComment = asyncHandler(async(req,res) => {
     const {videoId} = req.params;
@@ -73,4 +133,4 @@ const deleteComment = asyncHandler(async(req,res) => {
 
 
 
-export {addComment , updateComment , deleteComment}
+export {addComment , updateComment , deleteComment, getVideoComments}
